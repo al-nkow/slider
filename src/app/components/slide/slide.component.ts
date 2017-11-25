4d8f5b8cf4c23a830275e5c28ad296ff1927fdef
Aleksey Kovalchuk <al.nkow@gmail.com>
2017-11-25 21:25:17 +0300

    init

---
+++ src/app/components/slide/slide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.scss']
+})
+export class SlideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
