c0c86445219c7f5971c630dd996db6d123e7b0a6
Aleksey Kovalchuk <al.nkow@gmail.com>
2017-11-27 23:28:42 +0300

    button

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'slide',
-  templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.scss']
+    selector: 'slide',
+    templateUrl: './slide.component.html',
+    styleUrls: ['./slide.component.scss']
 })
 export class SlideComponent implements OnInit {
 
-  constructor() { }
+    shield = false;
 
-  ngOnInit() {
-  }
+    constructor() {
+
+    }
+
+    clickBut() {
+        this.shield = !this.shield;
+        console.log('Hi!!!', this.shield);
+    }
+
+    ngOnInit() {
+        // document.getElementById("trigger").addEventListener('click', function() {
+        //     console.log('TEST');
+        //     document.getElementById("ani-circle").beginElement();
+        // });
+        // document.getElementById("trigger2").addEventListener('click', function() {
+        //     console.log('TEST');
+        //     document.getElementById("ani-circle2").beginElement();
+        // });
+        // document.getElementById("trigger3").addEventListener('click', function() {
+        //     console.log('TEST');
+        //     document.getElementById("ani-circle3").beginElement();
+        //     // document.getElementById("ani-circle4").beginElement();
+        // });
+    }
 
 }
